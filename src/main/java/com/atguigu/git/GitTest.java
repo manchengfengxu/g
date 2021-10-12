771fb4fb5e8a0cc5e2569f531c1c5f69f40f33bf
Yan <1376888281@qq.com>
2021-10-12 14:37:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Norman
+ * @create 2021-10-12 14:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
